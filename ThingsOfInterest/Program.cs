4073c6b68e34fd98240852e34225308ddf03d83b
Adrian Furculita <afurculita@macadamian.com>
2018-06-15 21:42:08 +0300

    Add solution with main console application and DataStructures

---
+++ ThingsOfInterest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ThingsOfInterest.DataStructures;
+
+namespace ThingsOfInterest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var avlTree = new GenericAvlTree<int, string>();
+
+            avlTree.Insert(5,"5");
+            avlTree.Insert(3, "3");
+            avlTree.Insert(7, "7");
+            avlTree.Insert(2, "2"); 
+            avlTree.Insert(10, "10");
+            avlTree.Insert(8, "8");
+            avlTree.Insert(1, "1");
+            avlTree.Delete(10);
+
+            foreach (var avlNode in avlTree) {
+                Console.WriteLine(avlNode);
+            }
+
+
+            AVL tree = new AVL();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(2);
+            tree.Add(10);
+            tree.Add(8);
+            tree.Add(1);
+            tree.Delete(10);
+            tree.DisplayTree();
+        }
+    }
+}
